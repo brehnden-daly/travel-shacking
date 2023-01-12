1f786f9c7016fcae35a2947362eedbc469ece5c0
Brehnden Daly <brehndenddaly@gmail.com>
2023-01-11 19:41:04 -0700

    Added dedicated styling file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Styles from './Styling';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={Styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
